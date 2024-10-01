bbdc0a2af0258ca78ef787e77aee509947d153fa
txuglassop <t.xuglassop@gmail.com>
2024-10-01 22:29:58 +1000

    Updated file to add support for executing conformal_map.py with start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Check if an argument is provided
 if [ -z "$1" ]; then
-    echo "Usage: ./start.sh [line|circle]"
+    echo "Usage: ./start.sh [l|c|m]"
     exit 1
 fi
 
@@ -11,7 +11,9 @@ if [ "$1" == "l" ]; then
     python3 plot_line.py
 elif [ "$1" == "c" ]; then
     python3 plot_circles.py
+elif [ "$1" == "m" ]; then
+    python3 conformal_map.py
 else
-    echo "Invalid argument. Use 'l' for 'line' or 'c' for 'circle'."
+    echo "Invalid argument. Use 'l' for 'line' or 'c' for 'circle' or 'm' for grid map."
     exit 1
 fi
